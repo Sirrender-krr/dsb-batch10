1e75bcadc218bd14d66b69478af2f903dd0d1db7
Sirrender-krr <nearamong@gmail.com>
2025-01-05 10:55:55 +0700

    Create SQL_initial_Project_questions.ml

---
+++ SQL/SQL_initial_Project_questions.ml
@@ -0,0 +1,4 @@
+#SQL Initial Project Questions
+1. Can you find out the total milk production for 2023? Your manager wants this information for the yearly report.
+What is the total milk production for 2023?
+1.
